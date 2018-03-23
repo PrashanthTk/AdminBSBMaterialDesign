a35b881910093441ace8b5b191488913df9abbe3
Prashanth TK <prashanth9c007@gmail.com>
2018-03-23 11:53:39 -0400

    viz_filters used for database data display in new window

--- js/viz_filters.js
+++ js/viz_filters.js
@@ -3,4 +3,42 @@ function getAdmissions()
 	var date1=$("#filterdate1").val();
 	var date2=$("#filterdate2").val();
 	console.log(date1+"\n"+date2);
+
+	//call database to get records into ajaxresponse
+	htmlcode="<table>";
+
+	var ajaxresponse={
+		"row1":"	"
+	}
+	businesscode(ajaxresponse,htmlcode);
+	
+			
+}
+function businesscode(ajaxresponse,htmlcode)
+{
+	// Method 1
+	//ajaxresponse = JSON.stringify(response);
+	/*$.each(ajaxdata, function() {
+		htmlcode+="<tr>";
+  $.each(this, function(k, v) {
+    /// do stuff
+    html+=row+"</td>";
+    localStorage.setItem(k, v);
+  })
+  htmlcode+="</tr>";
+  
+});
+html+="</table>";
+var newWindow = window.open();
+newWindow.document.write(html);
+*/
+
+
+	//Method 2
+	/*Object.keys(result).forEach(function(key) {
+		
+      var row = result[key];
+      
+      console.log(row.name)
+  });*/
 }
