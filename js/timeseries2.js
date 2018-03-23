11d2873eea2c2b9c9c14a58ec18c6b7710f76afe
Prashanth TK <prashanth9c007@gmail.com>
2018-03-23 09:24:41 -0400

    bugs with csv to charts and UI bugs

--- js/timeseries2.js
+++ js/timeseries2.js
@@ -1,41 +1,56 @@
-
 var dat2;
 var dat3;
-
-
-function getData()
+function handledata(dat2)
 {
-    $.ajax({
-    type: "GET",
-    url: "/ReadmissionAnalyzer/tes/testdat.csv",
-    dataType: "text",
-    success: function(data) {
-        //console.log(data.text) ;
-        
-         coordinates=preprocess(data);
-         xvalues=coordinates[0];
-         yvalues=coordinates[1];
-         console.log("\nI am here "+xvalues[0]+"\t"+xvalues[1]);
+    //xvalues=[0,1,2,3];
+    //yvalues=[5,2,2,6];
+    
+    xvalues=[];
+    yvalues=[];
+        for(i = 0; i < dat2.length; i++){
+        //html += "<tr>";
+        if(i==0)continue;
+        counter=0;
+        for(j = 0; j < dat2[i].length; j++){
+            if(counter==0)
+              xvalues.push(dat2[i][j]);
+            if(counter==1)
+              yvalues.push(dat2[i][j])
+            //html +="<td>"+dat2[i][j]+"</td>";
 
+            if(dat2[i][j].trim() == ""){
+                //empty += "cell "+i+" "+j+" is empty";
+                //empty += "<br />";
+            }
+            counter=counter+1;
+        }
+      
+        //html += "</tr>";
+    }
+    return [xvalues,yvalues];
+}
+
+
+function drawchart(x,y)
+{
+    alert("NCSV Changed")
 const tsctx = document.getElementById('time_series_chart').getContext('2d');
-
-
 const tsdata = {
     // Labels should be Date objects
     //labels: [new Date(2017, 08, 16), new Date(2017, 08, 17), new Date(2017, 08, 18)],
-    labels: xvalues,
+    labels:x,
     datasets: [{
         fill: false,
         label: 'Readmissions',
         //data: [280, 250, 340],
-        data:yvalues,
+        data:y,
         borderColor: '#fe8b36',
         backgroundColor: '#fe8b36',
         lineTension: 0,
     }]
 }
 const options = {
-    type: 'line',
+    type: 'bar',
     data: tsdata,
     options: {
         fill: false,
@@ -64,77 +79,26 @@ const options = {
 }
 const chart = new Chart(tsctx, options);
 
-    },
-    error:function(err){console.log(err)},
-});
-
-
-
-// array of objects
-$.ajax({
-    type: "GET",
-    url: "./tes/testdata.csv",
-    dataType: "text",
-    success: function(data) { dat3 = $.csv.toObjects(data);
-           }
-});
-
-    
 }
-function preprocess(data)
+function getData()
 {
-    dat2 = $.csv.toArrays(data);
-    resstr='';
-    tuple=[]
-    code="<Br>{}"
-   
-    empty = "";
-    html = "<table>";
-    counter=0
-    xvalues=[]
-    yvalues=[]
-    for(i = 0; i < dat2.length; i++){
-        html += "<tr>";
-        if(i==0)continue;
-        counter=0;
-        for(j = 0; j < dat2[i].length; j++){
-            if(counter==0)
-              xvalues.push(dat2[i][j]);
-            if(counter==1)
-              yvalues.push(dat2[i][j])
-            html +="<td>"+dat2[i][j]+"</td>";
-
-            if(dat2[i][j].trim() == ""){
-                empty += "cell "+i+" "+j+" is empty";
-                empty += "<br />";
-            }
-            counter=counter+1;
-        }
-      
-        html += "</tr>";
-    }
-    //============================
-    tuple=dat2[0].toString().split(" ")
-    //drawchart(xvalues,yvalues);
-    console.log(xvalues[0]+"$ "+xvalues[1]+"$"+xvalues[2])
-    /*$('#map').html(        'Length of dat2 is ' + dat2.length + '<br>' +
-        'Length of dat3 is ' + dat3.length + '<br>' +
-        'Is dat0 equal to dat2: ' + _.isEqual(dat0, dat2)+ '<br>' +
-        'Is dat1 equal to dat3: ' + _.isEqual(dat1, dat3)+'\n dat2 xvalues <br>'+html+"<br>end")*/
-    return [xvalues,yvalues];
-
-
-
+    $.ajax({
+    type: "GET",
+    url: "/ReadmissionAnalyzer/tes/testdata.csv",
+    dataType: "text",
+    success: function(data) {
+        dat2 = $.csv.toArrays(data);
+        coords=handledata(dat2);
+        drawchart(coords[0],coords[1]);
+    },
+    error: function(err){
+        console.log(err);
+    },});
 }
-
-$(document).ready(function() {
-
-
-});
 
 function tryutton()
 {
-    alert("here")
+    alert("I am here")
 const tsctx = document.getElementById('time_series_chart').getContext('2d');
 const tsdata = {
     // Labels should be Date objects
@@ -178,10 +142,3 @@ const options = {
 }
 const chart = new Chart(tsctx, options);
 }
-
-//}); // end document ready
-
-
-
-
-
---
+++ js/viz_filters.js
@@ -0,0 +1,6 @@
+function getAdmissions()
+{
+	var date1=$("#filterdate1").val();
+	var date2=$("#filterdate2").val();
+	console.log(date1+"\n"+date2);
+}
